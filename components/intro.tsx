a02b6aee986ac33b8e65b68609b6c0ba09fab8a9
Anil Acar <ostravba@gmail.com>
2024-01-10 09:49:59 +0100

    smooth scroll and header element arrangement

--- components/intro.tsx
+++ components/intro.tsx
@@ -11,7 +11,10 @@ import { FaGithubSquare } from 'react-icons/fa';
 
 export default function Intro() {
   return (
-    <section className="mb-28 max-w-[50rem] text-center sm:mb-0 ">
+    <section
+      id="home"
+      className="mb-28 max-w-[50rem] text-center sm:mb-0 scroll-margin-top: 7rem scroll-mt-28 "
+    >
       <div className="flex items-center justify-center">
         <div className="relative">
           <motion.div
