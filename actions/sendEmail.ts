505a160ee6276b35771ed410f7d4106ddd86bc4a
Anil Acar <ostravba@gmail.com>
2024-02-05 07:17:34 +0100

    form is done

--- actions/sendEmail.ts
+++ actions/sendEmail.ts
@@ -1,7 +1,8 @@
 'use server';
-
+import React from 'react';
 import { Resend } from 'resend';
 import { validateString, getErrorMessage } from '@/lib/utils';
+import ContactFormEmail from '@/email/contact-form-email';
 
 const resend = new Resend(process.env.RESEND_API_KEY);
 
@@ -30,7 +31,11 @@ export const sendEmail = async (formData: FormData) => {
       to: 'ostravba@gmail.com',
       subject: "Message from portfolio website's form",
       reply_to: senderEmail as string,
-      text: message as string,
+      // text: message as string,
+      react: React.createElement(ContactFormEmail, {
+        message: message as string,
+        senderEmail: senderEmail as string,
+      }),
     });
   } catch (error: unknown) {
     return {
--- components/contact.tsx
+++ components/contact.tsx
@@ -2,10 +2,11 @@
 
 import React from 'react';
 import SectionHeading from './section-heading';
-import { FaPaperPlane } from 'react-icons/fa';
 import { motion } from 'framer-motion';
 import useSectionInView from '@/lib/hooks';
 import { sendEmail } from '@/actions/sendEmail';
+import SubmitBtn from './submit-btn';
+import toast from 'react-hot-toast';
 
 export default function Contact() {
   const { ref } = useSectionInView('Contact', 0.75);
@@ -32,7 +33,13 @@ export default function Contact() {
       <form
         className="mt-10 flex flex-col"
         action={async (formData) => {
-          await sendEmail(formData);
+          const { data, error } = await sendEmail(formData);
+
+          if (error) {
+            toast.error(error);
+            return;
+          }
+          toast.success('Email sent succussfully!');
         }}
         // action={sendEmail} //this also works
       >
@@ -50,13 +57,7 @@ export default function Contact() {
           placeholder="Your message"
           maxLength={5000}
         />
-        <button
-          type="submit"
-          className=" group flex items-center justify-center gap-2 h-[3rem] w-[8rem] bg-gray-800 text-white rounded-full outline-none transition-all focus:scale-110 hover:scale-110 hover:bg-gray-950 active:scale-105 m-auto"
-        >
-          Submit{' '}
-          <FaPaperPlane className="text-xs opacity-70 transition-all group:hover:translate-x-1 group-hover:-translate-y-1 " />
-        </button>
+        <SubmitBtn />
       </form>
     </motion.section>
   );
